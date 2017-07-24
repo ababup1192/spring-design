e984c40b1896ed264255171491b333dcb596c560
abab <ababup1192@gmail.com>
2017-07-24 12:06:32 +0900

    Add Infrastructure code

--- src/main/java/org/ababup1192/springdesign/LoginController.java
+++ src/main/java/org/ababup1192/springdesign/LoginController.java
@@ -1,5 +1,8 @@
 package org.ababup1192.springdesign;
 
+import org.ababup1192.springdesign.exception.AgeLimitException;
+import org.ababup1192.springdesign.exception.InvalidNameException;
+import org.ababup1192.springdesign.exception.UserNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,15 +24,17 @@ public class LoginController {
 
     @PostMapping("/sign_up")
     public ResponseEntity<String> signUp(@RequestBody User user) {
+        System.out.println(isFirstLetter(user.getName(), 'J'));
         // Business Logic
-        if (user.getAge() >= 15) {
+        if (user.getAge() < 15) {
+            throw new AgeLimitException();
+        } else if (!isFirstLetter(user.getName(), 'J')) {
+            throw new InvalidNameException(user.getName().charAt(0));
+        } else {
             // Persistent
             users.add(user);
             // Response
             return ResponseEntity.ok("Register: " + user.getName());
-        } else {
-            // Response
-            throw new AgeLimitException();
         }
     }
 
@@ -37,8 +42,12 @@ public class LoginController {
     public ResponseEntity<String> signIn(@RequestBody String name) {
         // Persistent
         return users.stream().filter(u -> u.getName().equals(name)).findFirst().
-                        // Response
+                // Response
                         map(u -> ResponseEntity.ok(u.getName())).
                         orElseThrow(() -> new UserNotFoundException(name));
+    }
+
+    private boolean isFirstLetter(String str, char firstLetter) {
+        return str.charAt(0) == firstLetter;
     }
 }
